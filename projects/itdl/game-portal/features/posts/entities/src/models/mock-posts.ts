d6371c88e1bd5ee0a5f84f4c23524e89a79d0e52
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-03-10 22:04:30 +0000

    change images source

--- projects/itdl/game-portal/features/posts/entities/src/models/mock-posts.ts
+++ projects/itdl/game-portal/features/posts/entities/src/models/mock-posts.ts
@@ -1,28 +1,29 @@
 // mock-posts.ts
 
-import { IGamePost } from "./post.model";
+import { IGamePost } from './post.model';
 
 // Mocked list of posts
 export const MOCK_POSTS: IGamePost[] = [
-  {
-    id: 1,
-    name: 'Post 1',
-    content: 'This is the content of post 1.',
-    imageUrl: 'https://example.com/image1.jpg',
-    createdDate: new Date('2023-01-01'),
-  },
-  {
-    id: 2,
-    name: 'Post 2',
-    content: 'This is the content of post 2.',
-    imageUrl: 'https://example.com/image2.jpg',
-    createdDate: new Date('2023-01-02'),
-  },
-  {
-    id: 3,
-    name: 'Post 3',
-    content: 'This is the content of post 3.',
-    imageUrl: 'https://example.com/image3.jpg',
-    createdDate: new Date('2023-01-03'),
-  },
+    {
+        id: 1,
+        name: 'Post 1',
+        content: 'This is the content of post 1.',
+        imageUrl: 'https://random.imagecdn.app/150/150',
+        createdDate: new Date('2023-01-01'),
+    },
+    {
+        id: 2,
+        name: 'Post 2',
+        content: 'This is the content of post 2.',
+        imageUrl: 'https://random.imagecdn.app/150/150',
+        createdDate: new Date('2023-01-02'),
+    },
+    {
+        id: 3,
+        name: 'Post 3',
+        content: 'This is the content of post 3.',
+        imageUrl: 'https://random.imagecdn.app/150/150',
+        createdDate: new Date('2023-01-03'),
+    },
 ];
+
